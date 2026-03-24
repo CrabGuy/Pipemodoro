aebcfe6ad4afa5cdc89d8984b54c7dfc9c4d0416
CrabGuy <bruno.direnzo2@gmail.com>
2026-03-24 19:12:45 +0100

    Started setting up svelte project for the frontend and installed m3 and the correct font

---
+++ apps/web/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { Button } from "m3-svelte";
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<Button onclick={() => console.log("Hello world")}>Click me</Button>
---
+++ apps/web/vite.config.js
@@ -0,0 +1,8 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+import { functionsMixins } from "vite-plugin-functions-mixins";
+import { tokenShaker } from "vite-plugin-token-shaker";
+
+export default defineConfig({
+  plugins: [sveltekit(), functionsMixins({ deps: ["m3-svelte"] }), tokenShaker()],
+});
