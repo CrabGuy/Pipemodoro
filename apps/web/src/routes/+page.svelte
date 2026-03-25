821f9d353a444595b791d50489d78f8183870e46
CrabGuy <bruno.direnzo2@gmail.com>
2026-03-25 10:52:08 +0100

    understood how svelte works better and started changing the theme to fit rose pine colors. Decent enough for now

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,8 +1,6 @@
-<script>
-  import { Button } from "m3-svelte";
+<script lang="ts">
+    import { Button, ConnectedButtons } from "m3-svelte";
+    let itemA = $state()
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
-
-<Button onclick={() => console.log("Hello world")}>Click me</Button>
+<Button variant="tonal" onclick={() => console.log("Clicked")}>Click me!</Button>
